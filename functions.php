54a8564f6e47094b4adfa0aaa884652c26a10f89
matthewvolk <volkmattj@gmail.com>
2018-08-05 21:15:41 -0700

    first commit

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php 
+  function codeschool_files() {
+    wp_enqueue_script('main_codeschool_js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
+    wp_enqueue_style('google_fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style('font_awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('codeschool_global_styles', get_stylesheet_uri());
+  }
+
+  add_action('wp_enqueue_scripts', 'codeschool_files');
+
+  function codeschool_features() {
+    register_nav_menu('mainNavigation', 'Main Navigation');
+    register_nav_menu('footerLocationOne', 'Footer Location One');
+    register_nav_menu('footerLocationTwo', 'Footer Location Two');
+    add_theme_support('title-tag');
+  }
+
+  add_action('after_setup_theme', 'codeschool_features');
+?>
