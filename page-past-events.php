f9784fe62e582a78e4ad066ded95c2abceda7b7a
matthewvolk <volkmattj@gmail.com>
2018-08-09 19:57:31 -0700

    add page for past events with custom wordpress query, add pagination to that page, and create link to that page in normal events archive

--- page-past-events.php
+++ page-past-events.php
@@ -5,18 +5,36 @@
   <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
   <div class="page-banner__content container container--narrow">
     
-    <h1 class="page-banner__title">Events</h1>
+    <h1 class="page-banner__title">Past Events</h1>
 
     <div class="page-banner__intro">
-      <p>See what's going on at Code School!</p>
+      <p>A recap of our past events.</p>
     </div>
   </div>  
 </div>
 
 <div class="container container--narrow page-section">
   <?php 
-    while(have_posts()) {
-      the_post(); ?>
+    $today = date('Ymd');
+    $pastEvents = new WP_Query(array(
+      'paged' => get_query_var('paged', 1),
+      'posts_per_page' => 10,
+      'post_type' => 'event',
+      'meta_key' => 'event_date',
+      'orderby' => 'meta_value_num',
+      'order' => 'ASC',
+      'meta_query' => array(
+        array(
+          'key' => 'event_date',
+          'compare' => '<=',
+          'value' => $today,
+          'type' => 'numeric'
+        )
+      )
+    ));
+
+    while($pastEvents->have_posts()) {
+      $pastEvents->the_post(); ?>
 
       <div class="event-summary">
         <a class="event-summary__date t-center" href="#">
@@ -32,10 +50,10 @@
     <?php } ?>
 
     <!-- Pagination -->
-    <?php echo paginate_links(); ?>
+    <?php echo paginate_links(array(
+      'total' => $pastEvents->max_num_pages
+    )); ?>
     
-    <hr class="section-break">
-    <p>Looking for a recap of past events? <a href="<?php site_url('/past-events') ?>">Click here.</a></p>
 </div>
 
 
