601e4a7faf07f390c9cf48bce9e9924c3a6a00ea
matthewvolk <volkmattj@gmail.com>
2018-08-09 19:42:25 -0700

    edit default archive-events query to match front page

--- functions.php
+++ functions.php
@@ -17,4 +17,22 @@
 
   add_action('after_setup_theme', 'codeschool_features');
 
+  function codeschool_adjust_queries($query) {
+    if (!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()) {
+      $today = date('Ymd');
+      $query->set('meta_key', 'event_date');
+      $query->set('orderby', 'meta_value_num');
+      $query->set('order', 'ASC');
+      $query->set('meta_query', array(
+        array(
+          'key' => 'event_date',
+          'compare' => '>=',
+          'value' => $today,
+          'type' => 'numeric'
+        )
+      ));
+    }
+  }
+
+  add_action('pre_get_posts', 'codeschool_adjust_queries');
 ?>
