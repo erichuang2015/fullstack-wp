078fff81f15cd8685ffbcf39aa6ac336f23be368
matthewvolk <volkmattj@gmail.com>
2018-08-09 17:28:01 -0700

    add excerpts for blog and event post types, add events main navigation link

--- header.php
+++ header.php
@@ -21,9 +21,9 @@
             <ul>
               <li><a href="<?php echo site_url('/about-us'); ?>">About Us</a></li>
               <li><a href="#">Programs</a></li>
-              <li><a href="#">Events</a></li>
+              <li><a href="<?php echo site_url('/events') ?>">Events</a></li>
               <li><a href="#">Campuses</a></li>
-              <li><a href="<?php echo site_url('blog'); ?>">Blog</a></li>
+              <li><a href="<?php echo site_url('/blog'); ?>">Blog</a></li>
             </ul>
           </nav>
           <div class="site-header__util">
