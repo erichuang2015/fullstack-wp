fce346d295a1b5b32a3d0af6762cfcb4afbfbac6
matthewvolk <volkmattj@gmail.com>
2018-08-11 14:13:00 -0700

    style professor list on program page

--- single-professor.php
+++ single-professor.php
@@ -16,8 +16,13 @@
 
     <div class="container container--narrow page-section">
 
+
+
       <div class="generic-content">
-        <?php the_content(); ?>
+        <div class="row group">
+          <div class="one-third"><?php the_post_thumbnail(); ?></div>
+          <div class="two-thirds"><?php the_content(); ?></div>
+        </div>
       </div>
 
       <?php 
--- single-program.php
+++ single-program.php
@@ -44,11 +44,20 @@
           echo '<hr class="section-break">';
           echo '<h2 class="headline headline--medium">' . get_the_title() . ' Professors</h2>';
 
+          echo '<ul class="professor-cards">';
           while($relatedProfessors->have_posts()) {
             $relatedProfessors->the_post(); ?>
-            <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
-          <?php } wp_reset_postdata();
+            <li class="professor-card__list-item">
+              <a class="professor-card" href="<?php the_permalink(); ?>">
+                <img class="professor-card__image" src="<?php the_post_thumbnail_url(); ?>">
+                <span class="professor-card__name"><?php the_title(); ?></span>
+              </a>
+            </li>
+          <?php } 
+          echo '</ul>';
         }
+
+        wp_reset_postdata();
 
         $today = date('Ymd');
         $homepageEvents = new WP_Query(array(
