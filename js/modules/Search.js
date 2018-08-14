b0e13b6b8caebafea5203a3f8cd10b1032a1085d
matthewvolk <volkmattj@gmail.com>
2018-08-13 21:22:36 -0700

    prepare user keyboard actions to handle eventual JSON requests to wordpress server

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -1,12 +1,15 @@
 class Search {
   // Create Object
   constructor() {
+    this.resultsDiv = jQuery(".search-overlay__results");
     this.openButton = jQuery('.js-search-trigger');
     this.closeButton = jQuery('.search-overlay__close');
     this.searchOverlay = jQuery('.search-overlay');
     this.searchField = jQuery('#search-term')
     this.events();
     this.isOverlayOpen = false;
+    this.isSpinnerVisible = false;
+    this.previousValue;
     this.typingTimer;
   }
 
@@ -15,18 +18,36 @@ class Search {
     this.openButton.on('click', this.openOverlay.bind(this));
     this.closeButton.on('click', this.closeOverlay.bind(this));
     jQuery(document).on('keydown', this.keyPressDispatcher.bind(this));
-    this.searchField.on('keydown', this.typingLogic.bind(this));
+    this.searchField.on('keyup', this.typingLogic.bind(this));
   }
 
   // Methods
   typingLogic() {
-    clearTimeout(this.typingTimer);
-    this.typingTimer = setTimeout(function() { console.log("2 seconds went by!"); }, 2000);
+    if (this.searchField.val() != this.previousValue) {
+      clearTimeout(this.typingTimer);
+
+      if (this.searchField.val()) {
+        if (!this.isSpinnerVisible) {
+          this.resultsDiv.html('<div class="spinner-loader"></div>');
+          this.isSpinnerVisible = true;
+        }
+        this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
+      } else {
+        this.resultsDiv.html("");
+        this.isSpinnerVisible = false;
+      }      
+    }
+    this.previousValue = this.searchField.val();
+  }
+
+  getResults() {
+    this.resultsDiv.html("<b>Wow!</b> Such excitement...");
+    this.isSpinnerVisible = false;
   }
 
   keyPressDispatcher(e) {
 
-    if(e.keyCode == 83 && !this.isOverlayOpen) {
+    if(e.keyCode == 83 && !this.isOverlayOpen && !jQuery("input, textarea").is(":focus")) {
       this.openOverlay();
     }
 
