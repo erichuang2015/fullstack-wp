41f351604eaf580be91f3445bfb73730dc0be119
matthewvolk <volkmattj@gmail.com>
2018-08-13 20:50:37 -0700

    add search results divider on search overlay

--- footer.php
+++ footer.php
@@ -68,6 +68,12 @@
         <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
       </div>
     </div>
+
+    <div class="container">
+      <div class="search-overlay__results">
+        
+      </div>
+    </div>
   </div>
 
   <?php wp_footer(); ?>
