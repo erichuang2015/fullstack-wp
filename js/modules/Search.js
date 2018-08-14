86df51ddce360895062fc88485fc29497a499619
matthewvolk <volkmattj@gmail.com>
2018-08-13 20:35:39 -0700

    add key press dispatcher

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -5,21 +5,38 @@ class Search {
     this.closeButton = jQuery('.search-overlay__close');
     this.searchOverlay = jQuery('.search-overlay');
     this.events();
+    this.isOverlayOpen = false;
   }
 
   // Events
   events() {
     this.openButton.on('click', this.openOverlay.bind(this));
     this.closeButton.on('click', this.closeOverlay.bind(this));
+    jQuery(document).on('keydown', this.keyPressDispatcher.bind(this));
   }
 
   // Methods
+  keyPressDispatcher(e) {
+
+    if(e.keyCode == 83 && !this.isOverlayOpen) {
+      this.openOverlay();
+    }
+
+    if(e.keyCode == 27 && this.isOverlayOpen) {
+      this.closeOverlay();
+    }
+  }
+
   openOverlay() {
     this.searchOverlay.addClass('search-overlay--active');
+    jQuery('body').addClass('body-no-scroll');
+    this.isOverlayOpen = true;
   };
 
   closeOverlay() {
     this.searchOverlay.removeClass('search-overlay--active');
+    jQuery('body').removeClass('body-no-scroll');
+    this.isOverlayOpen = false;
   };
 }
 
