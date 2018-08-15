f22f156cc0a8223bd5d6df8c8069afd9fa6d8025
matthewvolk <volkmattj@gmail.com>
2018-08-14 17:59:31 -0700

    take HTML for overlay out of footer in case of disabled JS, add methods to clear search term on overlay close, add methods to focus on search input if overlay opens

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -1,6 +1,7 @@
 class Search {
   // Create Object
   constructor() {
+    this.addSearchHTML();
     this.resultsDiv = jQuery(".search-overlay__results");
     this.openButton = jQuery('.js-search-trigger');
     this.closeButton = jQuery('.search-overlay__close');
@@ -31,7 +32,7 @@ class Search {
           this.resultsDiv.html('<div class="spinner-loader"></div>');
           this.isSpinnerVisible = true;
         }
-        this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
+        this.typingTimer = setTimeout(this.getResults.bind(this), 750);
       } else {
         this.resultsDiv.html("");
         this.isSpinnerVisible = false;
@@ -67,6 +68,8 @@ class Search {
   openOverlay() {
     this.searchOverlay.addClass('search-overlay--active');
     jQuery('body').addClass('body-no-scroll');
+    this.searchField.val('');
+    setTimeout(() => this.searchField.focus(), 301);
     this.isOverlayOpen = true;
   };
 
@@ -75,6 +78,26 @@ class Search {
     jQuery('body').removeClass('body-no-scroll');
     this.isOverlayOpen = false; 
   };
+
+  addSearchHTML() {
+    jQuery("body").append(`
+      <div class="search-overlay">
+        <div class="search-overlay__top">
+          <div class="container">
+            <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+            <input type="text" class="search-term" placeholder="What are you looking for?" id="search-term">
+            <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+          </div>
+        </div>
+
+        <div class="container">
+          <div class="search-overlay__results">
+            
+          </div>
+        </div>
+      </div>
+    `);
+  }
 }
 
 var search = new Search();
