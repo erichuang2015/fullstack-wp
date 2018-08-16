0c1a8e19e97d0f5b13c9cb70c7d400163058eb0b
matthewvolk <volkmattj@gmail.com>
2018-08-15 18:03:25 -0700

    WP_Query and keyword searching, santize search input

--- inc/search-route.php
+++ inc/search-route.php
@@ -2,14 +2,28 @@
   add_action('rest_api_init', 'codeschool_register_search');
 
   function codeschool_register_search() {
-    register_rest_route('codeschool', 'search', array(
+    register_rest_route('codeschool/v1', 'search', array(
       'methods' => WP_REST_SERVER::READABLE,
       'callback' => 'codeschool_search_results'
     ));
   }
 
-  function codeschool_search_results() {
-    return 'Congrats!';
+  function codeschool_search_results($data) {
+    $professors = new WP_Query(array(
+      'post_type' => 'professor',
+      's' => sanitize_text_field($data['term'])
+    ));
+
+    $professorResults = array();
+
+    while($professors->have_posts()) {
+      $professors->the_post();
+      array_push($professorResults, array(
+        'title' => get_the_title()
+      ));
+    }
+
+    return $professorResults;
   }
 
 ?>
