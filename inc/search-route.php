f2cc01c435b934a716246d95cf585cfbf7f49712
matthewvolk <volkmattj@gmail.com>
2018-08-15 20:34:40 -0700

    create relationship between professor and program to display appropriate professors in search results for programs

--- inc/search-route.php
+++ inc/search-route.php
@@ -45,7 +45,8 @@
       if (get_post_type() == 'program') {
         array_push($results['programs'], array(
           'title' => get_the_title(),
-          'permalink' => get_the_permalink()
+          'permalink' => get_the_permalink(),
+          'id' => get_the_id()
         ));
       }
 
@@ -75,6 +76,41 @@
           'description' => $description
         ));
       }
+    }
+
+    if ($results['programs']) {
+
+      $programsMetaQuery = array(
+        'relation' => 'OR'
+      );
+  
+      foreach($results['programs'] as $item) {
+        array_push($programsMetaQuery, array(
+          'key' => 'related_program',
+          'compare' => 'LIKE',
+          'value' => '"' . $item['id'] . '"'
+        ));
+      }
+  
+      $programRelationshipQuery = new WP_Query(array(
+        'post_type' => 'professor',
+        'meta_query' => $programsMetaQuery
+      ));
+  
+      while ($programRelationshipQuery->have_posts()) {
+        $programRelationshipQuery->the_post();
+  
+        if (get_post_type() == 'professor') {
+          array_push($results['professors'], array(
+            'title' => get_the_title(),
+            'permalink' => get_the_permalink(),
+            'image' => get_the_post_thumbnail_url(0, 'professorLandscape')
+          ));
+        }
+      }
+  
+      $results['professors'] = array_values(array_unique($results['professors'], SORT_REGULAR));
+
     }
 
     return $results;
