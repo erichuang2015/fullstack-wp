1bc861fbb37c8a6f132e3d2b93f7bc95259b4093
matthewvolk <volkmattj@gmail.com>
2018-08-15 18:45:59 -0700

    format javascript search results overlay to show general info, programs, campuses, professors, events

--- inc/search-route.php
+++ inc/search-route.php
@@ -9,21 +9,61 @@
   }
 
   function codeschool_search_results($data) {
-    $professors = new WP_Query(array(
-      'post_type' => 'professor',
+    $mainQuery = new WP_Query(array(
+      'post_type' => array('post', 'page', 'professor', 'program', 'campus', 'event'),
       's' => sanitize_text_field($data['term'])
     ));
 
-    $professorResults = array();
+    $results = array(
+      'generalInfo' => array(),
+      'professors' => array(),
+      'programs' => array(),
+      'events' => array(),
+      'campuses' => array()
+    );
 
-    while($professors->have_posts()) {
-      $professors->the_post();
-      array_push($professorResults, array(
-        'title' => get_the_title()
-      ));
+    while($mainQuery->have_posts()) {
+      $mainQuery->the_post();
+
+      if (get_post_type() == 'post' OR get_post_type() == 'page') {
+        array_push($results['generalInfo'], array(
+          'title' => get_the_title(),
+          'permalink' => get_the_permalink(),
+          'postType' => get_post_type(),
+          'authorName' => get_the_author()
+        ));
+      }
+
+      if (get_post_type() == 'professor') {
+        array_push($results['professors'], array(
+          'title' => get_the_title(),
+          'permalink' => get_the_permalink()
+        ));
+      }
+
+      if (get_post_type() == 'program') {
+        array_push($results['programs'], array(
+          'title' => get_the_title(),
+          'permalink' => get_the_permalink()
+        ));
+      }
+
+      if (get_post_type() == 'campus') {
+        array_push($results['campuses'], array(
+          'title' => get_the_title(),
+          'permalink' => get_the_permalink()
+        ));
+      }
+
+      if (get_post_type() == 'event') {
+        array_push($results['events'], array(
+          'title' => get_the_title(),
+          'permalink' => get_the_permalink()
+        ));
+      }
     }
 
-    return $professorResults;
+    return $results;
   }
 
 ?>
--- js/modules/Search.js
+++ js/modules/Search.js
@@ -42,20 +42,35 @@ class Search {
   }
 
   getResults() {
-    jQuery.when(
-      jQuery.getJSON(codeschoolData.root_url +  '/wp-json/wp/v2/posts?search=' + this.searchField.val()), 
-      jQuery.getJSON(codeschoolData.root_url +  '/wp-json/wp/v2/pages?search=' + this.searchField.val())
-    ).then((posts, pages) => {
-      var combinedResults = posts[0].concat(pages[0]);
+    jQuery.getJSON(codeschoolData.root_url + '/wp-json/codeschool/v1/search?term=' + this.searchField.val(), (results) => {
       this.resultsDiv.html(`
-        <h2 class="search-overlay__section-title">General Information</h2>
-        ${combinedResults.length ? '<ul class="link-list min-list">' : '<p>No General Information matches that search query.</p>'}
-          ${combinedResults.map(result => `<li><a href="${result.link}">${result.title.rendered}</a>${result.type == 'post' ? ` by ${result.authorName}` : '' }</li>`).join('')}
-        ${combinedResults.length ? '</ul>' : ''}
+        <div class="row">
+          <div class="one-third">
+            <h2 class="search-overlay__section-title">General Information</h2>
+            ${results.generalInfo.length ? '<ul class="link-list min-list">' : '<p>No general information matches that search query.</p>'}
+              ${results.generalInfo.map(result => `<li><a href="${result.permalink}">${result.title}</a>${result.postType == 'post' ? ` by ${result.authorName}` : '' }</li>`).join('')}
+            ${results.generalInfo.length ? '</ul>' : ''}
+          </div>
+          <div class="one-third">
+            <h2 class="search-overlay__section-title">Programs</h2>
+            ${results.programs.length ? '<ul class="link-list min-list">' : `<p>No programs match that search query. <a href="${codeschoolData.root_url}/programs">View all programs.</a></p>`}
+              ${results.programs.map(result => `<li><a href="${result.permalink}">${result.title}</a></li>`).join('')}
+            ${results.programs.length ? '</ul>' : ''}
+
+            <h2 class="search-overlay__section-title">Professors</h2>
+
+          </div>
+          <div class="one-third">
+            <h2 class="search-overlay__section-title">Campuses</h2>
+            ${results.campuses.length ? '<ul class="link-list min-list">' : `<p>No campuses match that search query. <a href="${codeschoolData.root_url}/campuses">View all campuses.</a></p>`}
+              ${results.campuses.map(result => `<li><a href="${result.permalink}">${result.title}</a></li>`).join('')}
+            ${results.campuses.length ? '</ul>' : ''}
+
+            <h2 class="search-overlay__section-title">Events</h2>
+
+          </div>
+        </div>
       `);
-      this.isSpinnerVisible = false;
-    }, () => {
-      this.resultsDiv.html('<p>Unexpected error, please try again.</p>')
     });
     this.isSpinnerVisible = false;
   }
