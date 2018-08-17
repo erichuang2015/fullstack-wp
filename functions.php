9a7818d2ae60efbc0a17131e2b159a1888776c34
matthewvolk <volkmattj@gmail.com>
2018-08-16 19:46:46 -0700

    remove admin bar for subscribers that are logged in

--- functions.php
+++ functions.php
@@ -111,4 +111,14 @@
       exit;
     }
   }
+
+  add_action('wp_loaded', 'no_subs_admin_bar');
+
+  function no_subs_admin_bar() {
+    $currentUser = wp_get_current_user();
+    
+    if (count($currentUser->roles) == 1 AND $currentUser->roles[0] == 'subscriber') {
+      show_admin_bar(false);
+    }
+  }
 ?>
