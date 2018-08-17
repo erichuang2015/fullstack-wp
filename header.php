5615b3af4ca34284d1d9e4340bbea47b8f6f5a4d
matthewvolk <volkmattj@gmail.com>
2018-08-16 19:41:03 -0700

    conditional logout button

--- header.php
+++ header.php
@@ -27,8 +27,16 @@
             </ul>
           </nav>
           <div class="site-header__util">
+          <?php if (is_user_logged_in()) { ?>
+
+            <a href="<?php echo wp_logout_url(); ?>" class="btn btn--small btn--white float-left">Log Out</a>
+
+          <?php } else { ?>
+           
             <a href="#" class="btn btn--small btn--white float-left push-right">Login</a>
-            <a href="#" class="btn btn--small btn--white float-left">Sign Up</a>
+            <a href="<?php echo esc_url(site_url('/wp-signup.php')) ?>" class="btn btn--small btn--white float-left">Sign Up</a>
+            
+          <?php } ?>
             <a href="<?php echo esc_url(site_url('/search')); ?>" class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></a>
           </div>
         </div>
