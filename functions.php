b7ad63cb3e74946540b36d3205ac109595d8ad90
matthewvolk <volkmattj@gmail.com>
2018-08-16 19:55:05 -0700

    style login page wp-login.php

--- functions.php
+++ functions.php
@@ -121,4 +121,23 @@
       show_admin_bar(false);
     }
   }
+
+  add_filter('login_headerurl', 'out_header_url');
+
+  function our_header_url() {
+    return esc_url(siteurl('/'));
+  }
+
+  add_action('login_enqueue_scripts', 'our_login_css');
+
+  function our_login_css() {
+    wp_enqueue_style('codeschool_global_styles', get_stylesheet_uri());
+    wp_enqueue_style('google_fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+  }
+
+  add_filter('login_headertitle', 'our_login_title');
+
+  function our_login_title() {
+    return get_bloginfo('name');
+  }
 ?>
